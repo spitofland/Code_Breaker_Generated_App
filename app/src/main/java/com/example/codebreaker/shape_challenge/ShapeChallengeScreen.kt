94f2621a7a6242c0c81d613aa6eef8696edb7526
kyle <spitofland@live.com>
2026-01-27 22:17:06 -0500

    Manual changes to fix shape challenge state.

--- app/src/main/java/com/example/codebreaker/shape_challenge/ShapeChallengeScreen.kt
+++ app/src/main/java/com/example/codebreaker/shape_challenge/ShapeChallengeScreen.kt
@@ -2,18 +2,15 @@ package com.example.codebreaker.shape_challenge
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -36,7 +33,8 @@ fun ShapeChallengeScreen(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Spacer(modifier = Modifier.height(16.dp))
-            ShapeGrid(state = viewModel.uiState.value)
+            val uiState = viewModel.uiState.collectAsState().value
+            ShapeGrid(state = uiState)
         }
         Spacer(modifier = Modifier.height(8.dp))
         Row(
